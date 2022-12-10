831b549a4589fd696b8715e3ca412aef63ab5adf
Sahide <65617524+sahideseker@users.noreply.github.com>
2022-12-11 02:32:33 +0300

    Create ReadigFromKeyboard.java

---
+++ ReadigFromKeyboard.java
@@ -0,0 +1,14 @@
+package first;
+
+import java.util.*;
+
+public class First {
+
+    public static void main(String[] args) {
+        String name;
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println ("May I know your Name: ");
+        name = sc.nextLine();
+        
+        System.out.println("Hello " + " " + name);
