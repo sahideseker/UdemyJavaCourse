496ce7dd56b411e2afd42a7467695ef553c83a54
Sahide <65617524+sahideseker@users.noreply.github.com>
2022-12-25 18:43:51 +0300

    Update AreaOfTriangle.java

--- AreaOfTriangle.java
+++ AreaOfTriangle.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Udemy {
 
 	public static void main(String[] args)
-	{ Scanner s = new Scanner (System.in);
+	{ 
+	Scanner s = new Scanner (System.in);
 	float base , height;
 	System.out.println("enter the base length:");
 	base  = s.nextFloat();
@@ -15,3 +16,27 @@ public class Udemy {
 	}
 
 }
+
+/*
+import java.util.Scanner;
+
+public class MyFirst {
+
+	public static void main(String[] args)
+	{ 
+	Scanner s = new Scanner (System.in);
+	int a,b,c;
+	float d,area;
+	System.out.println("enter 3 sides of a triangle");
+	a = s.nextInt();
+	b = s.nextInt();
+	c = s.nextInt();
+
+	d= (a+b+c)/2f;
+	
+	area= (float) Math.sqrt(d*(d-a)*(d-b)*(d-c));
+	
+	System.out.println("area is:" + area);
+	}
+}
+*/
