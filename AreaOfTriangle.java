9ee22286a44493c82cd5b90cfca92bdb5780a4be
Sahide <65617524+sahideseker@users.noreply.github.com>
2022-12-25 18:26:51 +0300

    Update AreaOfTriangle.java

--- AreaOfTriangle.java
+++ AreaOfTriangle.java
@@ -6,9 +6,9 @@ public class Udemy {
 	{ Scanner s = new Scanner (System.in);
 	float base , height;
 	System.out.println("enter the base length:");
-	base  = s.nextInt();
+	base  = s.nextFloat();
 	System.out.println("enter the height:"  );
-	height = s.nextInt();
+	height = s.nextFloat();
 	
 	float area = base*height/2; 
 	System.out.println("Area of Triangle is:" +  " " + area  );
